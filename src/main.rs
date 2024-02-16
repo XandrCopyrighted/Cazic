07387ba4b4db3bdcf5dc11cbd2d384c1b44f4864
XDR <111686716+XandrCopyrighted@users.noreply.github.com>
2024-02-16 12:49:41 -0500

    i'm stressed srry, there is no need to optimize this code
    :<

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 #[tauri::command]
-fn main() {
-    let remove_linux = thread::spawn(|| {
-    Command::new("/bin/sh")
+async fn main() {
+    tauri::Builder::default()
+       .run(tauri::generate_context!())
+       .expect("error while running tauri application");
+};
+let remove_linux = Command::new("/bin/sh")
        .args(["-c", "rm", "$HOME/.local/share/com.xdr.cazic/", "$HOME/.local/share/cazic"])
        .output()
        .expect("Failed to remove unnecessary files")
 });
-write!(remove_linux)   
-
-    tauri::Builder::default()
-       .run(tauri::generate_context!())
-       .expect("error while running tauri application");
-}
 write!(remove_linux)
+}
