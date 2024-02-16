59b8dbcf2e6c94bae4fa98c66aed80fd7cf53bf4
XDR <111686716+XandrCopyrighted@users.noreply.github.com>
2024-02-16 12:31:39 -0500

    `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@
 
 #[tauri::command]
 fn main() {
-    tauri::Builder::default()
-       .run(tauri::generate_context!())
-       .expect("error while running tauri application");
-}
-
-let remove_linux = thread::spawn(|| {
-   Command::new("/bin/sh")
+    let remove_linux = thread::spawn(|| {
+    Command::new("/bin/sh")
        .args(["-c", "rm", "$HOME/.local/share/com.xdr.cazic/", "$HOME/.local/share/cazic"])
        .output()
        .expect("Failed to remove unnecessary files")
 });
+write!(remove_linux)   
+
+    tauri::Builder::default()
+       .run(tauri::generate_context!())
+       .expect("error while running tauri application");
+}
 write!(remove_linux)
