c65b6dcb1baa2707f076102f3d5788e65d464e7b
xandr. <xxdr@duck.com>
2024-06-17 00:14:27 -0500

    refactor: DiscordRPC.js; Remove the `startDiscordRPC` func [CI SKIP]

--- src_frontend/javascript/discordRPC.js
+++ src_frontend/javascript/discordRPC.js
@@ -1,19 +1,15 @@
 const invoke = window.__TAURI__.invoke;
 
 function startDiscordRPC() {
-    setDiscordRPCSong();
-    invoke("start_rpc_thread");
-}
-
-function setDiscordRPCSong() {
     const currentTrack = queue[currentIndex];
     const metadata = {
         songName: currentTrack.title,
-        artist: currentTrack.artist,
+        artist: currentTrack.artist
         /* album: currentTrack.album,
         image: currentTrack.image */
     };
     invoke("set_song", metadata);
+    invoke("start_rpc_thread");
 }
 
 function stopDiscordRPC() {
